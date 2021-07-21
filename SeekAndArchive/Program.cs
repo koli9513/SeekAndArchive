a4f56da4e6b934d824653e54a996f135c7bdd082
koli9513 <66993009+koli9513@users.noreply.github.com>
2021-07-21 11:03:21 +0200

    feat: add menu

--- SeekAndArchive/Program.cs
+++ SeekAndArchive/Program.cs
@@ -1,9 +1,56 @@
-﻿namespace SeekAndArchive
+﻿using System;
+
+namespace SeekAndArchive
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            PrintMenu();
+            SelectMenu();
+        }
+
+        private static void SelectMenu()
+        {
+            var chosenMenu = Console.ReadLine();
+            switch (chosenMenu)
+            {
+                case "1":
+                    Search();
+                    break;
+                case "2":
+                    ShowSearchHistory();
+                    break;
+                case "3":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid menu option, choose again.");
+                    PrintMenu();
+                    SelectMenu();
+                    break;
+            }
+        }
+
+        private static void Search()
+        {
+            throw new NotImplementedException();
+        }
+        
+        private static void ShowSearchHistory()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PrintMenu()
+        {
+            Console.WriteLine(@"
+---------------------
+Please choose a menu item:
+    1 - New search
+    2 - Browse search history
+    3 - Exit
+---------------------");
         }
     }
 }
